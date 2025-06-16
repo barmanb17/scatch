7256a4f029931f93bec978b9d66a695f0b4ef72f
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-16 23:59:30 +0530

    routes updated

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -6,5 +6,11 @@ const ownerModel = require("../models/owner-model");
 router.get("/", (req, res) => {
     res.send("owners route working");
 })
+if(process.env.NODE_ENV === "development") {
+    router.post("/create", (req, res) => {
+    res.send("hey its working /create");
+});
+}
+
 
 module.exports = router;
