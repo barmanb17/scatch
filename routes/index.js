d09d5b638002c8dfe09d9f19a9857a7409aa098d
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-17 14:35:47 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const router = express.Router();
-const userModel = require("../models/user-model");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+const express    = require("express");
+const router     = express.Router();
+const userModel  = require("../models/user-model");
+const bcrypt     = require("bcrypt");
+const jwt        = require("jsonwebtoken");
 
 router.get("/", (req, res) => {
     res.send("index");
@@ -14,24 +14,24 @@ router.post("/register", async (req, res) => {
 
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(password, salt, async (err, hash) => {
-            if(err) return res.send(err.message);
-             else {
-                  if (!email || !fullname || !password) {
-            return res.status(400).send("All fields are required");
-        }
-        const user = await userModel.create({
-            email,
-            password: hash,
-            fullname,
-        });
+                if (err) return res.send(err.message);
+                else {
+                    if (!email || !fullname || !password) {
+                        return res.status(400).send("All fields are required");
+                    }
 
-        let token = jwt.sign({email, id: user._id}, "hehehehe");
-        res.cookie("token", token);
-        res.send("user created successfully");
+                    const user = await userModel.create({
+                        email,
+                        password: hash,
+                        fullname,
+                    });
+
+                    let token = jwt.sign({ email, id: user._id }, "hehehehe");
+                    res.cookie("token", token);
+                    res.send("user created successfully");
                 }
-            })
-        })
-
+            });
+        });
     } catch (err) {
         console.error(err);
         res.status(500).send("Server error");
