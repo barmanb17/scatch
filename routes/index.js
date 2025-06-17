0bae339069672491a981bc7b633a6b4fb38045df
Bijoy Barman <bijoy20-127@aec.ac.in>
2025-06-17 14:23:32 +0530

    jwt token bcrypt added

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+router.get("/", (req, res) => {
+    res.send("index");
+});
+
+router.post("/register", async (req, res) => {
+    try {
+        const { email, fullname, password } = req.body;
+
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, async (err, hash) => {
+            if(err) return res.send(err.message);
+             else {
+                  if (!email || !fullname || !password) {
+            return res.status(400).send("All fields are required");
+        }
+        const user = await userModel.create({
+            email,
+            password: hash,
+            fullname,
+        });
+
+        let token = jwt.sign({email, id: user._id}, "hehehehe");
+        res.cookie("token", token);
+        res.send("user created successfully");
+                }
+            })
+        })
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server error");
+    }
+});
+
+module.exports = router;
